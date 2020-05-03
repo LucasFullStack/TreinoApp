087ce508817e410bb5392942578a71578eb41170
Lucas Dias <lucas.dias@magically.com.br>
2020-05-03 13:12:28 -0300

    register

--- src/app/content/pages/auth/register/register-routing.module.ts
+++ src/app/content/pages/auth/register/register-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PagesPage } from './pages.page';
+import { RegisterPage } from './register.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: PagesPage
-  },
-
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+    component: RegisterPage
   }
 ];
 
@@ -19,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PagesPageRoutingModule {}
+export class RegisterPageRoutingModule {}
