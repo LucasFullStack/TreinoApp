13641e6ada894749feb54d3149c4f573d7aed0de
Lucas Dias <lucas.dias@magically.com.br>
2020-05-15 07:26:36 -0300

    correção offline manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { NetworkService, ConnectionStatus } from './core/services/network/network.service';
 import { OfflineManagerService } from './core/services/offline-manager/offline-manager.service';
+import { AuthenticationService } from './core/services/auth/authentication.service';
+import { AlertService } from './core/services/alert/alert.service';
 
 
 @Component({
@@ -17,7 +19,9 @@ export class AppComponent {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private networkService: NetworkService,
-    private offlineManager: OfflineManagerService
+    private offlineManager: OfflineManagerService,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService
   ) {
     this.initializeApp();
   }
@@ -27,10 +31,19 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
       this.networkService.onNetworkChange().subscribe((status: ConnectionStatus) => {
-        if (status == ConnectionStatus.Online) {
-          this.offlineManager.checkForEvents().subscribe();
-        }
+        this.authenticationService.isAuthorized()
+        .subscribe((isAuth) => {
+          if (status == ConnectionStatus.Online && isAuth) {
+            this.offlineManager.checkForEvents()
+            .toPromise()
+            .catch(()=> this.presentAlert() )
+          }
+        });
       });
     });
+  }
+  
+  presentAlert(){
+    this.alertService.presentErrorAlertDefault("Atenção!","Não foi possível terminar de sincronizar os dados. Caso o problema persista entre em contato com o administrador!")
   }
 }
